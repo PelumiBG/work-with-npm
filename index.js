5b01298088c1c799b6e7883301cf30f86c65b13f
Pelumi <bellojoshua2019@gmail.com>
2025-09-17 07:54:31 -0600

    index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ import chalk from 'chalk';
 import { checkAge } from './ageCheck.js';
 import { formatUserInfo } from './userinfo.js';
 
-console.log(chalk.red('What is your name?'));
+console.log(chalk.red('What is your DOB?'));
 
 // Using process.stdin to get user input
 stdin.on('data', (nameInput) => {
